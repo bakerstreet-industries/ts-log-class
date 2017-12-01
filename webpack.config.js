c0a18452e86556750c0dbeb14f1a05555728e6bb
Russ Watson <russ@bakerstreet.industries>
2017-12-01 08:31:43 -0600

    Adding bundling via webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.ts',
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+      '.json',
+      '.ts',
+      '.tsx'
+    ]
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  module: {
+    loaders: [
+      { test: /\.ts(x?)$/, loader: 'ts-loader' },
+    ],
+  },
+};
