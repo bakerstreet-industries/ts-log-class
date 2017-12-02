5b4d2409a58847adaecc482971c03b4bd03158ae
Russ Watson <russ@bakerstreet.industries>
2017-12-01 19:34:48 -0600

    TDD; implemented ILogger options.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
 
-export default function log(): ((target) => void) {
+export default function log(opts: ILogOptions = null): ((target) => void) {
+  if (!opts) {
+    opts = {};
+  }
+  if (!opts.hook) {
+    opts.hook = defaultHook;
+  }
+  if (!opts.out) {
+    opts.out = console.log;
+  }
   return function (target: any): void {
     let pt = target.prototype;
     Object.keys(pt).forEach(key => {
       let fn: IPatchedMethod = applyisMethod(pt[key]);
       if (!fn.isPatched && fn.isAMethod) {
-        pt[key] = applyMonkeyPatch(fn, key);
+        pt[key] = applyMonkeyPatch(fn, key, opts);
       }
     });
   };
+}
+
+export interface ILogOptions {
+  hook?: (logProps: IHookProperties) => string;
+  out?: (message?: any, ...optionalParams: any[]) => void;
+}
+
+export interface IHookProperties {
+  timestamp: number;
+  className: string;
+  arguments: string[];
+  properties: { [property: string]: any };
+  result: any;
 }
 
 interface IPatchedMethod extends Function {
@@ -23,11 +45,38 @@ function applyisMethod(allegedFn: IPatchedMethod): IPatchedMethod {
   return allegedFn;
 }
 
-function applyMonkeyPatch(method: IPatchedMethod, methodName: string): Function {
+function applyMonkeyPatch(method: IPatchedMethod, methodName: string, opts: ILogOptions): Function {
   method.isPatched = true;
+  console.log('What is this?1', this);
   return (...rest): any => {
     let result = method.apply(this, rest);
-    result += ' - MONKEYED';
+    console.log('What is this?2', this);
+    waitForResult(result).then(val => {
+      console.log('What is this?3', this);
+      opts.out(
+        opts.hook({
+          arguments: [],
+          className: this.constructor ? this.constructor.name : '[UNKNOWN_CLASS_NAME]',
+          properties: [],
+          result: val,
+          timestamp: Date.now(),
+        })
+      );
+    });
     return result;
   }
+}
+
+function defaultHook(props: IHookProperties): string {
+  return "loooooooooooooool";
+}
+
+function waitForResult(result): Promise<any> {
+  return new Promise(resolve => {
+    if (result instanceof Promise) {
+      result.then(val => resolve(val));
+    } else {
+      resolve(result);
+    }
+  });
 }
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,15 +1,28 @@
 import chai = require("chai");
+import spies = require("chai-spies")
 import log from "../src/main";
 
-@log()
-class MockClass {
-    public doStuff(): string {
-        return "Mock did stuff!";
-    }
+chai.use(spies);
+
+class BaseMockClass {
+  public doStuff(): string {
+    return "Mock did stuff!";
+  }
+  public doAsyncStuff(): Promise<{ sample: string }> {
+    return Promise.resolve({
+      sample: 'output'
+    });
+  }
 }
 
 describe("ts-logger", () => {
-    it("Should Monkey patch the output", () => {
-        chai.expect(new MockClass().doStuff()).to.equal("Mock did stuff! - MONKEYED");
-    });
+  it("Should Log output to console.log", done => {
+    let spy = chai.spy.on(console, 'log');
+    new BaseMockClass().doStuff();
+    chai.expect(spy).to.not.have.been.called();
+
+    new BaseMockClass().doAsyncStuff()
+      .then(val => chai.expect(spy).to.not.have.been.called())
+      .then(() => done());
+  });
 });
