d94c029eb5ab8143b69f7e218ce236f734802de8
Russ Watson <russ@bakerstreet.industries>
2017-12-20 20:09:47 -0600

    no message

--- .webpack/tslogclass.js
+++ .webpack/tslogclass.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["tslogclass"] = factory();
+		exports["ts-log-class"] = factory();
 	else
-		root["tslogclass"] = factory();
+		root["ts-log-class"] = factory();
 })(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
